a352c35586da04e71e4cd8d729964e742d9a167c
casperkids <yukishiroi@protonmail.com>
2024-02-19 12:42:04 +0100

    update Favorite List

--- script.js
+++ script.js
@@ -60,9 +60,57 @@ statusButtons.forEach(button => {
 
 let statusFavButtons = document.querySelectorAll('.statusFavButton')
   
-  statusFavButtons.forEach(button => {
-    button.addEventListener('click', changeFavStatus)
+statusFavButtons.forEach(button => {
+  button.addEventListener('click', changeFavStatus)
+})
+
+
+//Fetch
+const getBooks = async () => {
+  let response = await fetch('http://openlibrary.org/subjects/art.json')
+  let data = await response.json()
+
+  data.works.forEach((book) =>{
+  // Build cards using JS (repeat):
+  let cardDiv = document.createElement('div');
+    
+  // Add styling
+  cardDiv.style.maxWidth = '700px';
+  cardDiv.classList.add('container', 'card', 'mb-3');
+        
+    
+  // Add content for the card:
+  cardDiv.innerHTML = `<div class="row g-0">
+   <div class="col-md-4 d-flex align-items-center">
+    <img src="https://covers.openlibrary.org/b/id/${book.cover_id}-M.jpg" alt="${book.cover_id}" style="max-width: 80%;" >
+   </div>
+      
+   <div class="col-md-8">
+    <div class="card-body px-2">
+      <h3 class="card-title">${book.title}</h3>
+      <h5>${book.authors[0].name}</h5>
+      <p>${book.first_publish_year}</p>
+      <p class="card-text">${book.subject[0]}</p>
+      
+            
+        <div class="container">
+          <div class="row">
+              <div>
+              <p>${book.public_scan ? 'You can scan' : 'You cannot scan' }</p>
+              </div> 
+            
+          </div>
+          <div class="w-100"></div>
+            <div class="card-footer w-100 text-muted">${book.subject[0]}</div>
+        </div>
+      </div>`;
+      // Append the card to the .collection-list
+      document.querySelector('.openLibruary-List').append(cardDiv);
   })
+}
+getBooks()
+console.log( 'After async');
+
 
 // Favorite Books
 let favoriteBooksListDiv = document.createElement('div');
@@ -93,6 +141,6 @@ favoriteBooks.forEach((book) => {
         </div>
       </div>
     </div>`;
-  
-    favoriteBooksListDiv.appendChild(cardDiv);
+    document.querySelector('.favorite-List').append(cardDiv);
+    //favoriteBooksListDiv.appendChild(cardDiv);
 });
